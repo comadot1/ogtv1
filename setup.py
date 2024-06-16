c5e45690a0c3e4149a133ed093725b71102c6bf2
comadot1 <55697835+comadot1@users.noreply.github.com>
2024-06-16 20:15:53 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setting = {
     'home_module': None,
     'menu': {
         'uri': __package__,
-        'name': 'tvchak',
+        'name': 'ogtv',
         'list': [
             {
                 'uri': 'main/setting',
